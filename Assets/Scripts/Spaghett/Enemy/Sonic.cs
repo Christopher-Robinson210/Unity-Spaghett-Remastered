0978d67c8de92b001c68bac17342eeb6c8cdfc62
Christopher Robinson <chriserino210@protonmail.com>
2021-11-02 17:12:25 -0500

    Updated Sonic.cs - Teleport Effect
    Sonic now visually telegraphs that he is about to teleport.

--- Assets/Scripts/Spaghett/Enemy/Sonic.cs
+++ Assets/Scripts/Spaghett/Enemy/Sonic.cs
@@ -8,6 +8,11 @@ namespace Spaghett
     {
         public AudioClip moveClip;
         public AudioClip noMoveClip;
+        private bool doJump;
+        private void Start()
+        {
+            doJump = DoJump();
+        }
         public override void Move()
         {
             if (!isCollide)
@@ -28,39 +33,11 @@ namespace Spaghett
             IsAlive();
             if (isAlive)
             {
-                if (DoJump())
+                if (doJump)
                 {
-                    if ((transform.position - target.transform.position).magnitude > 2.5f)
-                    {
-                        bool hasMoved = false;
-                        int loops = 0;
-                        while (!hasMoved)
-                        {
-                            if (loops < 25)
-                            {
-                                Vector3 sonicPosition = new Vector3(Random.Range((Mathf.Abs(transform.position.x) * -1), Mathf.Abs(transform.position.x)), Random.Range((Mathf.Abs(transform.position.y) * -1), Mathf.Abs(transform.position.y)), 0f);
-                                if ((sonicPosition - target.transform.position).magnitude < 2f)
-                                {
-                                    loops++;
-                                    continue;
-                                }
-                                else
-                                {
-                                    transform.position = sonicPosition;
-                                    audioSource.PlayOneShot(moveClip);
-                                }
-                            }
-                            else
-                            {
-                                break;
-                            }
-                        }
-                    }
-                    else
-                    {
-                        transform.position = new Vector3((transform.position.x * -1), (transform.position.y * -1), 0f);
-                        audioSource.PlayOneShot(moveClip);
-                    }
+                    StartCoroutine(DelayTP());
+                    // decide how to teleport
+                    //DoTeleport();
                 }
                 else
                 {
@@ -75,12 +52,62 @@ namespace Spaghett
             StartCoroutine(DelayFlip());
         }
 
+        void DoTeleport()
+        {
+            if (doJump)
+            {
+                if ((transform.position - target.transform.position).magnitude > 2.5f)
+                {
+                    bool hasMoved = false;
+                    int loops = 0;
+                    while (!hasMoved)
+                    {
+                        if (loops < 50)
+                        {
+                            Vector3 sonicPosition = new Vector3(Random.Range((Mathf.Abs(transform.position.x) * -1), Mathf.Abs(transform.position.x)), Random.Range((Mathf.Abs(transform.position.y) * -1), Mathf.Abs(transform.position.y)), 0f);
+                            if ((sonicPosition - target.transform.position).magnitude < 2f)
+                            {
+                                loops++;
+                                continue;
+                            }
+                            else
+                            {
+                                transform.position = sonicPosition;
+                                audioSource.PlayOneShot(moveClip);
+                                hasMoved = true;
+                                doJump = false;
+                            }
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+                }
+                else
+                {
+                    transform.position = new Vector3((transform.position.x * -1), (transform.position.y * -1), 0f);
+                    audioSource.PlayOneShot(moveClip);
+                    doJump = false;
+                }
+            }
+        }
+
         IEnumerator DelayFlip()
         {
             SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
             spriteRenderer.flipX = true;
             yield return new WaitForSeconds(0.25f);
             spriteRenderer.flipX = false;
+        }
+
+        IEnumerator DelayTP()
+        {
+            Vector3 temp = transform.localScale;
+            transform.localScale = new Vector3(transform.localScale.x + .03f, transform.localScale.y + .03f, 0f);
+            yield return new WaitForSecondsRealtime(0.35f);
+            transform.localScale = temp;
+            DoTeleport();
         }
     }
 }
